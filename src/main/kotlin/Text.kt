539e6822d53ef1721eaecfce775fb1987953e1e4
Laurence Gonsalves <laurence@xenomachina.com>
2017-03-07 12:19:01 -0800

    Move terminal code to com.xenomachina.text.term

--- src/main/kotlin/Text.kt
+++ src/main/kotlin/Text.kt
@@ -16,10 +16,11 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this library; if not, see http://www.gnu.org/licenses/
 
-package com.xenomachina.common
+package com.xenomachina.text
 
 import kotlin.coroutines.experimental.buildSequence
 
+// TODO: remove this
 /**
  * Splits [String] into a [Sequence] of lines. Newlines are included on any lines that are terminated by one.
  */
@@ -51,124 +52,6 @@ fun StringBuilder.clear() {
     this.setLength(0)
 }
 
-internal const val SPACE_WIDTH = 1
-
-fun String.padTo(width: Int): String {
-    val sb = StringBuilder()
-    var lineWidth = 0
-    var singleLine = true
-    for (codePoint in codePointSequence()) {
-        if (codePoint == '\n'.toInt()) {
-            singleLine = false
-            while (lineWidth < width) {
-                sb.append(" ")
-                lineWidth += SPACE_WIDTH
-            }
-            sb.append("\n")
-            lineWidth = 0
-        } else {
-            sb.appendCodePoint(codePoint)
-            lineWidth += codePointWidth(codePoint)
-        }
-    }
-    if (singleLine || lineWidth > 0) {
-        while (lineWidth < width) {
-            sb.append(" ")
-            lineWidth += SPACE_WIDTH
-        }
-    }
-    return sb.toString()
-}
-
-internal const val NBSP_CODEPOINT = 0xa0
-
-fun String.wrapText(maxWidth: Int): String {
-    val sb = StringBuilder()
-    val word = StringBuilder()
-    var lineWidth = 0
-    var wordWidth = 0
-    fun handleSpace() {
-        if (wordWidth > 0) {
-            if (lineWidth > 0) {
-                sb.append(" ")
-                lineWidth += SPACE_WIDTH
-            }
-            sb.append(word)
-            lineWidth += wordWidth
-            word.clear()
-            wordWidth = 0
-        }
-    }
-    for (inputCodePoint in codePointSequence()) {
-        if (Character.isSpaceChar(inputCodePoint) && inputCodePoint != NBSP_CODEPOINT) {
-            // space
-            handleSpace()
-        } else {
-            // non-space
-            val outputCodePoint = if (inputCodePoint == NBSP_CODEPOINT) ' '.toInt() else inputCodePoint
-            val charWidth = codePointWidth(outputCodePoint).toInt()
-            if (lineWidth > 0 && lineWidth + SPACE_WIDTH + wordWidth + charWidth > maxWidth) {
-                sb.append("\n")
-                lineWidth = 0
-            }
-            if (lineWidth == 0 && lineWidth + SPACE_WIDTH + wordWidth + charWidth > maxWidth) {
-                // Eep! Word would be longer than line. Need to break it.
-                sb.append(word)
-                word.clear()
-                wordWidth = 0
-                sb.append("\n")
-                lineWidth = 0
-            }
-            word.appendCodePoint(outputCodePoint)
-            wordWidth += charWidth
-        }
-    }
-    handleSpace()
-
-    return sb.toString()
-}
-
-/**
- * Returns an estimated cell width of a Unicode code point when displayed on a monospace terminal.
- * Possible return values are -1, 0, 1 or 2. Control characters (other than null) and Del return -1.
- *
- * This function is based on the public domain [wcwidth.c](https://www.cl.cam.ac.uk/~mgk25/ucs/wcwidth.c)
- * written by Markus Kuhn.
- */
-fun codePointWidth(ucs: Int): Int {
-    // 8-bit control characters
-    if (ucs == 0) return 0
-    if (ucs < 32 || (ucs >= 0x7f && ucs < 0xa0)) return -1
-
-    // Non-spacing characters. This is simulating the binary search of
-    // `uniset +cat=Me +cat=Mn +cat=Cf -00AD +1160-11FF +200B`.
-    if (ucs != 0x00AD) { // soft hyphen
-        val category = Character.getType(ucs).toByte()
-        if (category == Character.ENCLOSING_MARK || // "Me"
-                category == Character.NON_SPACING_MARK || // "Mn"
-                category == Character.FORMAT || // "Cf"
-                (0x1160 <= ucs && ucs <= 0x11FF) || // Hangul Jungseong & Jongseong
-                ucs == 0x200B) // zero width space
-            return 0
-    }
-
-    // If we arrive here, ucs is not a combining or C0/C1 control character.
-    return if (ucs >= 0x1100 && (ucs <= 0x115f || // Hangul Jamo init. consonants
-            ucs == 0x2329 || ucs == 0x232a ||
-            (ucs >= 0x2e80 && ucs <= 0xa4cf && ucs != 0x303f) || // CJK ... Yi
-            (ucs >= 0xac00 && ucs <= 0xd7a3) || // Hangul Syllables
-            (ucs >= 0xf900 && ucs <= 0xfaff) || // CJK Compatibility Ideographs
-            (ucs >= 0xfe10 && ucs <= 0xfe19) || // Vertical forms
-            (ucs >= 0xfe30 && ucs <= 0xfe6f) || // CJK Compatibility Forms
-            (ucs >= 0xff00 && ucs <= 0xff60) || // Fullwidth Forms
-            (ucs >= 0xffe0 && ucs <= 0xffe6) ||
-            (ucs >= 0x20000 && ucs <= 0x2fffd) ||
-            (ucs >= 0x30000 && ucs <= 0x3fffd)))
-        2 else 1
-}
-
-fun String.codePointWidth(): Int = codePointSequence().sumBy { codePointWidth(it) }
-
 fun String.trimNewline(): String {
     if (endsWith('\n')) {
         return substring(0, length - 1)
@@ -177,34 +60,3 @@ fun String.trimNewline(): String {
     }
 }
 
-fun columnize(vararg s: String, minWidths: IntArray? = null): String {
-    val columns = Array(s.size) { mutableListOf<String>() }
-    val widths = Array(s.size) { 0 }
-    for (i in 0..s.size - 1) {
-        if (minWidths != null && i < minWidths.size) {
-            widths[i] = minWidths[i]
-        }
-        for (line in s[i].lineSequence()) {
-            val cell = line.trimNewline()
-            columns[i].add(cell)
-            widths[i] = widths[i].coerceAtLeast(cell.codePointWidth())
-        }
-    }
-    val height = columns.maxBy { it.size }?.size ?: 0
-    val sb = StringBuilder()
-    for (j in 0..height - 1) {
-        var lineWidth = 0
-        var columnStart = 0
-        for (i in 0..columns.size - 1) {
-            columns[i].getOrNull(j)?.let { cell ->
-                for (k in 1..columnStart - lineWidth) sb.append(" ")
-                lineWidth = columnStart
-                sb.append(cell)
-                lineWidth += cell.codePointWidth()
-            }
-            columnStart += widths[i]
-        }
-        sb.append("\n")
-    }
-    return sb.toString()
-}
---
+++ src/test/kotlin/TextTermTest.kt
@@ -0,0 +1,180 @@
+// Copyright © 2017 Laurence Gonsalves
+//
+// This file is part of xenocom, a library which can be found at
+// http://github.com/xenomachina/xenocom
+//
+// This library is free software; you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published by the
+// Free Software Foundation; either version 2.1 of the License, or (at your
+// option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+// for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this library; if not, see http://www.gnu.org/licenses/
+
+package com.xenomachina.text.term
+
+import io.kotlintest.specs.FunSpec
+import org.junit.Assert.assertEquals
+
+class StringPadToTest : FunSpec() {
+    init {
+        test("empty") {
+            assertEquals("          ", "".padTo(10))
+        }
+
+        test("one small line") {
+            assertEquals("abc       ", "abc".padTo(10))
+        }
+
+        test("one large line") {
+            assertEquals("0123456789abcde", "0123456789abcde".padTo(10))
+        }
+
+        test("one small line with newline") {
+            assertEquals("abc       \n", "abc\n".padTo(10))
+        }
+
+        test("one large line with newline") {
+            assertEquals("0123456789abcde\n", "0123456789abcde\n".padTo(10))
+        }
+
+        test("multiline") {
+            assertEquals(
+                    "abc       \nde fg     \n0123456789xyz\nfoo       \n",
+                    "abc\nde fg\n0123456789xyz\nfoo\n".padTo(10))
+        }
+
+        test("multiline without trailing newline") {
+            assertEquals(
+                    "abc       \nde fg     \n0123456789xyz\nfoo       ",
+                    "abc\nde fg\n0123456789xyz\nfoo".padTo(10))
+        }
+    }
+}
+
+class String_WrapTextTest : FunSpec() {
+    init {
+        test("empty") {
+            assertEquals("", "".wrapText(10))
+        }
+
+        test("simple") {
+            assertEquals("hello", "hello".wrapText(10))
+        }
+
+        test("multi-spaces are smushed") {
+            assertEquals("foo bar", "foo  bar".wrapText(10))
+        }
+
+        test("leading-spaces are removed") {
+            assertEquals("foo bar", " foo bar".wrapText(10))
+            assertEquals("foo bar", "  foo bar".wrapText(10))
+        }
+
+        test("trailing-spaces are removed") {
+            assertEquals("foo bar", "foo bar ".wrapText(10))
+            assertEquals("foo bar", "foo bar  ".wrapText(10))
+        }
+
+        test("words are wrapped") {
+            assertEquals(
+                    "foo bar\nbaz quux\nzarf",
+                    "foo bar baz quux zarf".wrapText(10))
+        }
+
+        test("words are wrapped at exactly the wrap margin") {
+            assertEquals(
+                    "foo bar ba\nz quux\nzarf",
+                    "foo bar ba z quux zarf".wrapText(10))
+        }
+
+        test("extra spaces don't push past wrap margin") {
+            assertEquals(
+                    "12 45 78 0\naa bb cc\ndd",
+                    "12  45  78  0  aa  bb  cc  dd".wrapText(10))
+        }
+
+        test("double-width chars take up double space") {
+            // if we treated 你好 as having a width of 2 then 'hi' would end up on first line.
+            assertEquals(
+                    "你好 is\n'hi' in\nChinese",
+                    "你好 is 'hi' in Chinese".wrapText(10))
+        }
+    }
+}
+
+class CodePointWidthTest : FunSpec() {
+    init {
+        test("nulchar == 0") {
+            assertEquals(0, codePointWidth(0))
+        }
+
+        test("control characters == -1") {
+            for (i in 1..(' '.toInt() - 1)) {
+                assertEquals("[$i]", -1, codePointWidth(i))
+            }
+        }
+
+        test("assorted single-width characters") {
+            assertEquals(1, codePointWidth(' '.toInt()))
+            assertEquals(1, codePointWidth('M'.toInt()))
+            assertEquals(1, codePointWidth('π'.toInt()))
+            assertEquals(1, codePointWidth('ʖ'.toInt()))
+        }
+
+        test("assorted double-width characters") {
+            assertEquals(2, codePointWidth('好'.toInt()))
+            assertEquals(2, codePointWidth('Ｍ'.toInt()))
+        }
+
+        test("assorted non-BMP characters and Emoji") {
+            assertEquals(1, codePointWidth(0x1f01c)) // Mahjong tile four of circles
+
+            // TODO: Should some Emoji return 2?
+            assertEquals(1, codePointWidth(0x1f601)) // Emoticons
+            assertEquals(1, codePointWidth(0x2702)) // Dingbats
+            assertEquals(1, codePointWidth(0x1f680)) // Transport & Map
+            assertEquals(1, codePointWidth(0x24c2)) // Enclosed
+            assertEquals(1, codePointWidth(0x1f170)) // Enclosed
+        }
+
+        test("string containing a few printing characters") {
+            assertEquals(5, "hello".codePointWidth()) // Mahjong tile four of circles
+            assertEquals(12, "hello = 你好".codePointWidth()) // Mahjong tile four of circles
+            // TODO: decide how control characters should be handled
+        }
+    }
+}
+
+class ColumnizeTest : FunSpec() {
+    init {
+        test("first column shorter, no minWidths") {
+            assertEquals("foobar\n   baz\n", columnize("foo\n", "bar\nbaz\n"))
+        }
+
+        test("second column shorter, no minWidths") {
+            assertEquals("barfoo\nbaz\n", columnize("bar\nbaz\n", "foo\n"))
+        }
+
+        test("first column shorter, with large minWidths") {
+            assertEquals("foo  bar\n     baz\n", columnize("foo\n", "bar\nbaz\n", minWidths = intArrayOf(5, 10)))
+        }
+
+        test("second column shorter, with large minWidths") {
+            assertEquals("bar  foo\nbaz\n", columnize("bar\nbaz\n", "foo\n", minWidths = intArrayOf(5, 10)))
+        }
+
+        test("first column shorter, with small minWidths") {
+            assertEquals("foobar\n   baz\n", columnize("foo\n", "bar\nbaz\n", minWidths = intArrayOf(2, 2)))
+        }
+
+        test("second column shorter, with small minWidths") {
+            assertEquals("barfoo\nbaz\n", columnize("bar\nbaz\n", "foo\n", minWidths = intArrayOf(2, 2)))
+        }
+    }
+}
